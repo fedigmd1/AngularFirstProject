5a3df46782629c75a1add0d7c321b4ee7f9fee3a
fedi.gmd <gammoudifedi@gmail.com>
2020-12-29 17:12:55 +0100

    fix appmodule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,9 @@ import { AlertModule } from 'ngx-bootstrap/alert';
 import { NavbarComponent } from './shared/navbar/navbar.component';
 import { LandingModule } from './LangingPage/landing.module';
 import { HomePageModule } from './HomeScreen/home-page.module';
+import { MovieService } from './services/movie.service';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { HttpClientModule } from '@angular/common/http'
 
 @NgModule({
   declarations: [
@@ -21,8 +24,11 @@ import { HomePageModule } from './HomeScreen/home-page.module';
     MDBBootstrapModule.forRoot(),
     LandingModule,
     HomePageModule,
+    FlexLayoutModule,
+    HttpClientModule,
+
   ],
-  providers: [],
+  providers: [MovieService],
 
   bootstrap: [AppComponent]
 })
