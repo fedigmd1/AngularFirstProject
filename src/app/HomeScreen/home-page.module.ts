54d3cc2f5c5a02a88c1529bcbb759245dda6d1fc
fedi.gmd <gammoudifedi@gmail.com>
2020-12-29 17:14:13 +0100

    fix home page module ( code and logic )

--- src/app/HomeScreen/home-page.module.ts
+++ src/app/HomeScreen/home-page.module.ts
@@ -2,23 +2,39 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CarouselComponent } from './carousel/carousel.component';
 
-
-
 import { BrowserModule } from '@angular/platform-browser';
 import { MDBBootstrapModule } from 'angular-bootstrap-md';
 import { AlertModule } from 'ngx-bootstrap/alert';
+import { MoviecardsComponent } from './moviecards/moviecards.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatCardModule} from '@angular/material/card';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { HttpClientModule } from '@angular/common/http'
+
 
 
 @NgModule({
-  declarations: [CarouselComponent],
+  declarations: [
+    CarouselComponent,
+    MoviecardsComponent,
+  ],
   imports: [
     CommonModule,
     MDBBootstrapModule,
     AlertModule,
     BrowserModule,
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatToolbarModule,
+    MatButtonModule,
+    FlexLayoutModule,
+    HttpClientModule,
   ],
   exports: [
     CarouselComponent,
+    MoviecardsComponent,
   ]
 })
 export class HomePageModule { }
