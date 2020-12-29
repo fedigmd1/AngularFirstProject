a3ea2493ceab24dc197b1182503a49a403137c88
fedi.gmd <gammoudifedi@gmail.com>
2020-12-29 17:12:28 +0100

    add movie service and its code

---
+++ src/app/services/movie.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { environment } from '../../environments/environment'
+import { Observable } from 'rxjs';
+
+import { Movie } from '../models/movie';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MovieService {
+  private uri: string = environment.api
+  private key: string = environment.key
+  private url: string = this.uri + "/movie/popular?api_key=" + this.key + "&language=fr&page=2";
+
+  constructor(private http: HttpClient) {
+  }
+  getPopularMovie(): Observable<any> {
+    return this.http.get<Movie>(this.url)
+  }
+
+  getImageFromApi(name: string) {
+    return 'https://image.tmdb.org/t/p/w300' + name
+  }
+}
